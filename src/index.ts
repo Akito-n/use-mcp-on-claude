c0da24da8ae8c6515180ae6f51dd90273fb76ef3
Akito-n <naki415@outlook.jp>
2025-03-17 17:03:08 +0900

    use configure

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
-import {
-  McpServer,
-  ResourceTemplate,
-} from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { configureServer } from "./obsidianMcpServer.js";
-import { z } from "zod";
+import { McpServer, ResourceTemplate } from '@modelcontextprotocol/sdk/server/mcp.js'
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
+import { configureServer as ObsidianConfigureServer } from './obsidianMcpServer'
+import { configureServer as KibelaConfigureServer } from './kibela'
+import { z } from 'zod'
 
 async function main() {
   const server = new McpServer({
-    name: "obsidian-search",
-    version: "1.0.0",
-  });
+    name: 'obsidian-search',
+    version: '1.0.0',
+  })
 
-  configureServer(server);
+  ObsidianConfigureServer(server)
+  KibelaConfigureServer(server)
 
-  const resources: Record<string, string> = {};
+  const resources: Record<string, string> = {}
 
   server.resource(
-    "greeting",
-    new ResourceTemplate("greeting://{name}", { list: undefined }),
+    'greeting',
+    new ResourceTemplate('greeting://{name}', { list: undefined }),
     async (uri, { name }) => {
-      resources[`greeting://${name}`] = `Hello, ${name}!`;
+      resources[`greeting://${name}`] = `Hello, ${name}!`
 
       return {
         contents: [
@@ -29,35 +28,31 @@ async function main() {
             text: `Hello, ${name}!`,
           },
         ],
-      };
+      }
+    },
+  )
+
+  server.tool('greet', { name: z.string().describe('The name to greet') }, async ({ name }) => {
+    const resourceUri = `greeting://${name}`
+
+    const greeting = resources[resourceUri] || (await generateGreeting(name))
+
+    return {
+      content: [{ type: 'text', text: greeting }],
     }
-  );
-
-  server.tool(
-    "greet",
-    { name: z.string().describe("The name to greet") },
-    async ({ name }) => {
-      const resourceUri = `greeting://${name}`;
-
-      const greeting = resources[resourceUri] || (await generateGreeting(name));
-
-      return {
-        content: [{ type: "text", text: greeting }],
-      };
-    }
-  );
+  })
 
   async function generateGreeting(name: string) {
-    return `Hello, ${name}! (generated directly)`;
+    return `Hello, ${name}! (generated directly)`
   }
 
-  const transport = new StdioServerTransport();
-  server.connect(transport);
+  const transport = new StdioServerTransport()
+  server.connect(transport)
 
-  process.stdin.resume();
+  process.stdin.resume()
 }
 
 main().catch((error) => {
-  console.error("Fatal error in main():", error);
-  process.exit(1);
-});
+  console.error('Fatal error in main():', error)
+  process.exit(1)
+})

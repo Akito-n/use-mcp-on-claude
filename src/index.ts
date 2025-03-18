eb3fe0754c5abdb0e18e42ffcbf7581d4f4b5def
Akito-n <naki415@outlook.jp>
2025-03-18 10:49:01 +0900

    add brave

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,18 @@
-import { McpServer, ResourceTemplate } from '@modelcontextprotocol/sdk/server/mcp.js'
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
 import { configureServer as ObsidianConfigureServer } from './obsidianMcpServer'
 import { configureServer as KibelaConfigureServer } from './kibela'
-import { z } from 'zod'
+import { configureServer as BraveSearchConfigureServer } from './braveSearch'
 
 async function main() {
   const server = new McpServer({
-    name: 'obsidian-search',
+    name: 'multi-tool-mcp-server',
     version: '1.0.0',
   })
 
   ObsidianConfigureServer(server)
   KibelaConfigureServer(server)
-
-  const resources: Record<string, string> = {}
-
-  server.resource(
-    'greeting',
-    new ResourceTemplate('greeting://{name}', { list: undefined }),
-    async (uri, { name }) => {
-      resources[`greeting://${name}`] = `Hello, ${name}!`
-
-      return {
-        contents: [
-          {
-            uri: uri.href,
-            text: `Hello, ${name}!`,
-          },
-        ],
-      }
-    },
-  )
-
-  server.tool('greet', { name: z.string().describe('The name to greet') }, async ({ name }) => {
-    const resourceUri = `greeting://${name}`
-
-    const greeting = resources[resourceUri] || (await generateGreeting(name))
-
-    return {
-      content: [{ type: 'text', text: greeting }],
-    }
-  })
-
-  async function generateGreeting(name: string) {
-    return `Hello, ${name}! (generated directly)`
-  }
+  BraveSearchConfigureServer(server)
 
   const transport = new StdioServerTransport()
   server.connect(transport)
